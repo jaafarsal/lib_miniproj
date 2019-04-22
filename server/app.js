ec9428e3fc851c74af8c27759bea551875086f35
Your Name <you@example.com>
2019-04-22 15:05:55 +0300

    server side structure

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-requested-With, Content-Type, Accept, Authorization');
+
+    next();
+});
+app.use(express.json());
+app.use('/api/courses', require('./api/routers/courses'));
+app.use('/api/users', require('./api/routers/users'));
+//app.use('/api/classes',require('./api/routers/classes'));
+app.use('/', (req, res) => {
+    res.status(404).send('Incorrect URLI');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, _=> console.log('Listening to port ' + PORT));
+module.exports = app;
