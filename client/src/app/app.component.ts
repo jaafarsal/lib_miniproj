2b8a81806f4bdc3421993db9a7bb3f15e69d6635
YuriAli <yuri.ali.mobail@gmail.com>
2019-04-27 13:19:42 +0300

    co

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {GlobalService} from "./global.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+  constructor(private configService: GlobalService) {
+      this.showConfig();
+  }
+  showConfig() {
+    this.configService.getConfig()
+        .subscribe((data => console.log(data)));
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { BookaddComponent } from './books/bookadd.component';
+
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import {HttpClientModule} from "@angular/common/http";
 import { RouterModule } from '@angular/router';
 import { AddbookComponent } from './addbook/addbook.component';
 import { FormsModule , ReactiveFormsModule } from '@angular/forms';
@@ -25,10 +24,12 @@ import { FormsModule , ReactiveFormsModule } from '@angular/forms';
     AddbookComponent
   ],
   imports: [
+    BrowserModule,
+    HttpClientModule,
+      AppRoutingModule,
     BrowserModule, AppRoutingModule,RouterModule,
     HttpClientModule,FormsModule,ReactiveFormsModule ,
       ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
-
   ],
   providers: [],
   bootstrap: [AppComponent]
