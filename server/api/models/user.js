d7fbcb6d0b84aadb163c1638d95492d9b94c866a
Your Name <you@example.com>
2019-04-27 11:35:19 +0300

    creating route and login, register component

--- server/api/models/user.js
+++ server/api/models/user.js
@@ -18,11 +18,27 @@ User.getUserById = (userId, result) => {
 };
 
 User.createUser = (newUser, result) => {
-
+    sql.query("insert into users (email, password, firstName, lastName) values(?, ?, ?, ?)", 
+    [newUser.email, newUser.password, newUser.firstName, newUser.lastName], (err, res) => {
+        
+        if(err){
+            console.log(err);
+            result(err, null);
+        }else {
+            result(null, res);
+        }
+    });
 };
 
 User.getUserByEmail = (email, result) => { 
-
+    sql.query("select * from users where email = ?", email, (err, res) => {
+        if(err){
+            result(err, null);
+        }else {
+            result(null, res);
+        }
+    })
 };
+
 
 module.exports = User;
