95c1bdeb766ff4f7decc9ddebf07a2949ea93dd2
Jaafarsal <jafarsal94@gmail.com>
2019-04-27 09:29:41 +0300

    angular word

--- server/api/routers/book.js
+++ server/api/routers/book.js
@@ -5,8 +5,30 @@ const jwt = require('jsonwebtoken');
 const privateKey = 'secret';
 const app = express();
 const router = express.Router();
+const books = require('../models/book');
 
+router.get('/' , (req,res) => {
+    books.getAllbooks((err,book) => {
+        if (err) {
+            res.status(500).json({error: err});
+        } else {
+            res.status(200).json(book);
+        }
+    })
+});
 
-    
+router.get('/addbook' , (req,res) => {
+    const book = {
+        isbn: 0, title: req.body.title, author: req.body.author,
+        keywords: req.body.keywords, publisher: req.body.publisher
+    };
+    books.addbook(book,(err,result) => {
+        if (err) {
+            res.status(500).json({error: err});
+        } else {
+            res.status(200).json({message: "Book added successfully" ,createdUser : book});
+        }
+    })
+});
 
 module.exports = router;
