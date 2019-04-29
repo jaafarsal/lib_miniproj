2a78d4ae79d1c54cc295f3cfbcbf736973e5c4f6
Abode113 <Abd.r.horani@gmail.com>
2019-04-29 14:09:57 +0300

    back up

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { RegisterComponent } from './register/register.component';
 import { BookaddComponent } from './books/bookadd.component';
 import { AddbookComponent } from './addbook/addbook.component';
 import { SearchResultComponent } from 'src/app/search-result/search-result.component';
+import { AuthorComponent } from './author/author.component';
 
 const routes: Routes = [
     {path: 'login', component: LoginComponent },
     {path: 'register', component: RegisterComponent},
+    {path: 'author', component: AuthorComponent},
     {path: 'books', component: BookaddComponent , data: { animation: 'heroes' } },
     {path: 'books/:key', component: SearchResultComponent },
     {path: 'booksadd', component: AddbookComponent , data: { animation: 'hero' }},
--- client/src/app/author/author.component.ts
+++ client/src/app/author/author.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../service/user.service';
+import {Router} from "@angular/router";
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-author',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthorComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private cookieService: CookieService) { }
+
+    checkLogingIn(){
+      if(this.cookieService.get('access_token')){
+        this.router.navigate(['/books']);
+      }else{
+        this.router.navigate(['/login']);
+      }
+    }
+
 
   ngOnInit() {
+    this.checkLogingIn();
   }
 
 }
